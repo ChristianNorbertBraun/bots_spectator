c06ff7ee8d5bd82ee3734bea98dfe848ea9c4b59
Christoph Dietze <christoph.dietze@kurzdigital.com>
2019-05-10 16:20:06 +0200

    Formatting

--- src/gl.js
+++ src/gl.js
@@ -22,33 +22,33 @@ function initBuffers(gl, program) {
             -1, -1,
             1, 1,
             1, -1]),
-        gl.STATIC_DRAW)
-    var uvBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer)
+        gl.STATIC_DRAW);
+    var uvBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
     // gl.bufferData(gl.ARRAY_BUFFER, new FA(calcUvCoords()), gl.STATIC_DRAW)
     // var vertexBufferLoc = getEnabledAttribLocation(program, 'p')
     // var uvBufferLoc = getEnabledAttribLocation(program, 'uv')
-    var perspectiveLoc = gl.getUniformLocation(program, 'perspective')
-    var transformationLoc = gl.getUniformLocation(program, 'transformation')
-    var textureLoc = gl.getUniformLocation(program, 'texture')
+    var perspectiveLoc = gl.getUniformLocation(program, 'perspective');
+    var transformationLoc = gl.getUniformLocation(program, 'transformation');
+    var textureLoc = gl.getUniformLocation(program, 'texture');
 }
 
 export function init() {
-    var gl = getContext()
-  //  var texture = createTextureFrom(atlas)
-    var program = gl.createProgram()
+    var gl = getContext();
+    //  var texture = createTextureFrom(atlas)
+    var program = gl.createProgram();
     gl.attachShader(program, compileShader(gl, gl.VERTEX_SHADER,
-        D.getElementById('VertexShader').textContent))
+        D.getElementById('VertexShader').textContent));
     gl.attachShader(program, compileShader(gl, gl.FRAGMENT_SHADER,
-        D.getElementById('FragmentShader').textContent))
-    gl.linkProgram(program)
-    gl.useProgram(program)
-    initBuffers(gl, program)
-    gl.enable(gl.BLEND)
-    gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA)
-    gl.clearColor(.898, .800, .505, 1)
+        D.getElementById('FragmentShader').textContent));
+    gl.linkProgram(program);
+    gl.useProgram(program);
+    initBuffers(gl, program);
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+    gl.clearColor(.898, .800, .505, 1);
 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     // wireInputs()
     // createDust()
     // createBlood()
