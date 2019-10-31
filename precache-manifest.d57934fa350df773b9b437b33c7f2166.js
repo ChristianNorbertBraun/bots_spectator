9a30a4be5e9f4682725b69f615a1ece4c3e910ea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-31 12:27:50 +0000

    Deploy ChristianNorbertBraun/bots_spectator to github.com/ChristianNorbertBraun/bots_spectator.git:gh-pages

---
+++ precache-manifest.d57934fa350df773b9b437b33c7f2166.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f14052edf41800cc71f4770c3679c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f07a3765f5b75bdea204",
+    "url": "/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "1607dad1ec4a2a7498ef",
+    "url": "/static/js/2.072fbf65.chunk.js"
+  },
+  {
+    "revision": "f07a3765f5b75bdea204",
+    "url": "/static/js/main.1d8f01c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
